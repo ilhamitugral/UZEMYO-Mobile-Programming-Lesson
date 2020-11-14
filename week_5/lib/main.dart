d468373ba5dc029f11ccef75b0e5fa73415016d5
Ilhami Tugral <ilhamitugral@gmail.com>
2020-11-14 22:39:09 +0300

    Arayüz güncelleştirildi. Responsive tasarım eklendi.

--- week_5/lib/main.dart
+++ week_5/lib/main.dart
@@ -20,155 +20,171 @@ class IlhamisApp extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     Color textColor = Color(0xFFFFFFFF);
-    double actionButtonWidth = 80.75;
     String phoneNumber = '+90 555 555 55 55';
     String personName = 'İlhami Tuğral';
+    double iconSize = 18;
 
     return Scaffold(
       backgroundColor: Color(0xFF000000),
       appBar: null,
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.only(top: 55, left: 28, right: 28),
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/user.png',
-                  width: 96,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 21, bottom: 21),
-                  child: Text(
-                    personName,
-                    style: TextStyle(
-                      color: textColor,
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                    ),
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(top: 24, left: 8, right: 8),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/user.png',
+                    width: 96,
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    MyBox(
-                        width: actionButtonWidth,
-                        icon: 'assets/images/speech-bubble.png',
-                        text: 'mesaj'),
-                    MyBox(
-                        width: actionButtonWidth,
-                        icon: 'assets/images/phone-call.png',
-                        text: 'ara'),
-                    MyBox(
-                        width: actionButtonWidth,
-                        icon: 'assets/images/whatsapp.png',
-                        text: 'whatsapp'),
-                    MyBox(
-                        width: actionButtonWidth,
-                        icon: 'assets/images/mail.png',
-                        text: 'e-posta'),
-                  ],
-                ),
-                Container(
-                  width: size.width,
-                  decoration: MyDecoration().get(),
-                  margin: EdgeInsets.only(top: 18),
-                  padding: EdgeInsets.fromLTRB(11, 10, 11, 10),
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          'cep',
-                          style: TextStyle(color: textColor),
-                        ),
-                      ),
-                      Text(
-                        phoneNumber,
-                        style: TextStyle(color: textColor),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  width: size.width,
-                  height: 44,
-                  decoration: MyDecoration().get(),
-                  margin: EdgeInsets.only(top: 18),
-                  padding: EdgeInsets.fromLTRB(11, 10, 11, 10),
-                  alignment: Alignment.centerLeft,
-                  child: Container(
+                  Container(
+                    margin: EdgeInsets.only(top: 21, bottom: 21),
                     child: Text(
-                      'Facetime',
+                      personName,
                       style: TextStyle(
                         color: textColor,
-                        fontSize: 14,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  width: size.width,
-                  height: 44,
-                  decoration: MyDecoration().get(),
-                  margin: EdgeInsets.only(top: 18),
-                  padding: EdgeInsets.fromLTRB(11, 10, 11, 10),
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    child: Text(
-                      'Notlar',
-                      style: TextStyle(color: textColor),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: size.width,
-                  decoration: MyDecoration().get(),
-                  margin: EdgeInsets.only(top: 18),
-                  padding: EdgeInsets.fromLTRB(11, 10, 11, 10),
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Container(
-                        child: Text(
-                          'Mesaj Gönder',
-                          style: TextStyle(color: textColor),
+                      Panel(
+                        iconSrc: Image.asset(
+                          'assets/images/speech-bubble.png',
+                          width: iconSize,
                         ),
+                        text: 'mesaj',
                       ),
-                      Container(
-                        margin: EdgeInsets.only(top: 7.5, bottom: 7.5),
-                        color: Color(0xFFA0A0A0),
-                        height: 1,
-                      ),
-                      Container(
-                        child: Text(
-                          'Kişiyi Paylaş',
-                          style: TextStyle(color: textColor),
+                      Panel(
+                        icon: Icon(
+                          Icons.call,
+                          size: iconSize,
+                          color: Color(0xFFFFFFFF),
                         ),
+                        text: 'ara',
                       ),
-                      Container(
-                        margin: EdgeInsets.only(top: 7.5, bottom: 7.5),
-                        color: Color(0xFFA0A0A0),
-                        height: 1,
-                      ),
-                      Container(
-                        child: Text(
-                          'Hızlı Aramaya Ekle',
-                          style: TextStyle(color: textColor),
+                      Panel(
+                        iconSrc: Image.asset(
+                          'assets/images/whatsapp.png',
+                          width: iconSize,
                         ),
+                        text: 'whatsapp',
                       ),
-                      Container(
-                        margin: EdgeInsets.only(top: 7.5, bottom: 7.5),
-                        color: Color(0xFFA0A0A0),
-                        height: 1,
+                      Panel(
+                        iconSrc: Image.asset(
+                          'assets/images/mail.png',
+                          width: iconSize,
+                        ),
+                        text: 'e-posta',
                       ),
                     ],
                   ),
-                )
-              ],
+                  Container(
+                    width: size.width,
+                    decoration: MyDecoration().get(),
+                    margin: EdgeInsets.only(top: 6),
+                    padding: EdgeInsets.fromLTRB(11, 10, 11, 10),
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'cep',
+                            style: TextStyle(color: textColor),
+                          ),
+                        ),
+                        Text(
+                          phoneNumber,
+                          style: TextStyle(color: textColor),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: size.width,
+                    height: 44,
+                    decoration: MyDecoration().get(),
+                    margin: EdgeInsets.only(top: 14),
+                    padding: EdgeInsets.fromLTRB(11, 10, 11, 10),
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      child: Text(
+                        'Facetime',
+                        style: TextStyle(
+                          color: textColor,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: size.width,
+                    height: 44,
+                    decoration: MyDecoration().get(),
+                    margin: EdgeInsets.only(top: 14),
+                    padding: EdgeInsets.fromLTRB(11, 10, 11, 10),
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      child: Text(
+                        'Notlar',
+                        style: TextStyle(color: textColor),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: size.width,
+                    decoration: MyDecoration().get(),
+                    margin: EdgeInsets.only(top: 14, bottom: 18),
+                    padding: EdgeInsets.fromLTRB(11, 10, 11, 10),
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            'Mesaj Gönder',
+                            style: TextStyle(color: textColor),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 7.5, bottom: 7.5),
+                          color: Color(0xFFA0A0A0),
+                          height: 1,
+                        ),
+                        Container(
+                          child: Text(
+                            'Kişiyi Paylaş',
+                            style: TextStyle(color: textColor),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 7.5, bottom: 7.5),
+                          color: Color(0xFFA0A0A0),
+                          height: 1,
+                        ),
+                        Container(
+                          child: Text(
+                            'Hızlı Aramaya Ekle',
+                            style: TextStyle(color: textColor),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 7.5, bottom: 7.5),
+                          color: Color(0xFFA0A0A0),
+                          height: 1,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -177,41 +193,52 @@ class IlhamisApp extends StatelessWidget {
   }
 }
 
-class MyBox extends StatelessWidget {
-  const MyBox({
+class Panel extends StatelessWidget {
+  const Panel({
     Key key,
     this.text,
     this.icon,
+    this.iconSrc,
     this.width = 0,
   }) : super(key: key);
 
   final String text;
-  final String icon;
+  final Icon icon;
+  final Image iconSrc;
   final double width;
 
   @override
   Widget build(BuildContext context) {
-    double _width =
-        this.width == 0 ? MediaQuery.of(context).size.width : this.width;
-    return Container(
-      width: _width,
-      margin: EdgeInsets.only(left: 4, right: 4),
-      padding: EdgeInsets.only(top: 7, bottom: 7),
-      decoration: MyDecoration().get(),
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(bottom: 8),
-            child: Image.asset(this.icon, width: 24),
+    return Expanded(
+      child: Container(
+        height: 65,
+        margin: EdgeInsets.only(left: 4, right: 4),
+        padding: EdgeInsets.only(top: 7, bottom: 7),
+        child: RaisedButton(
+          color: Color(0xFF272727),
+          onPressed: () {},
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
           ),
-          Text(
-            this.text,
-            style: TextStyle(
-              color: Color(0xFFFFFFFF),
-              fontSize: 12,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: this.icon != null ? this.icon : this.iconSrc,
+                ),
+                Container(
+                  child: Text(
+                    this.text,
+                    style: TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+        ),
       ),
     );
   }
